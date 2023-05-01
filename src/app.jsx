298b8542dbb8cb75c0bd3e2488597d21937134f1
yawarpwn <john.kazz@gmail.com>
2023-05-01 11:50:44 -0500

    nuevos cambios

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,24 @@
 import { useEffect, useState } from "preact/hooks"
 import confetti from "canvas-confetti"
+import Notify from "simple-notify"
+import "simple-notify/dist/simple-notify.min.css"
 import "./app.css"
 import { getQuestions } from "./services/getQuestions"
 
-function Option({ alternativa, onInputChange, index , userResponse }) {
+function Option({ alternativa, onInputChange, index, userResponse }) {
   const checked = userResponse === alternativa
   return (
     <div className="flex gap-2 items-center rounded-lg bg-white/5">
-      <label className="h-24 flex items-center ml-2 " htmlFor={`alternativa-${index}`}>
+      <label
+        className="h-24 flex items-center ml-2 "
+        htmlFor={`alternativa-${index}`}
+      >
         <div className="flex items-center gap-2">
           <input
             onChange={onInputChange}
             value={alternativa}
             id={`alternativa-${index}`}
-            className="inline-block"
+            className="inline-block w-3 h-3"
             type="radio"
             checked={checked}
           />
@@ -26,6 +31,27 @@ function Option({ alternativa, onInputChange, index , userResponse }) {
 
 function ListOfOptions({ question, checkAnswer, updateQuestion }) {
   const [userResponse, setUserResponse] = useState("")
+  const [error, setError] = useState(null)
+
+  function pushNotify() {
+    new Notify({
+      status: "error",
+      title: "Te Equivocastes",
+      text: "Prueba otra respuesta",
+      effect: "slide",
+      speed: 100,
+      customClass: null,
+      customIcon: null,
+      showIcon: true,
+      showCloseButton: true,
+      autoclose: true,
+      autotimeout: 3000,
+      gap: 20,
+      distance: 20,
+      type: 1,
+      position: "center",
+    })
+  }
 
   const onInputChange = (event) => {
     setUserResponse(event.target.value)
@@ -35,13 +61,14 @@ function ListOfOptions({ question, checkAnswer, updateQuestion }) {
     event.preventDefault()
 
     const isCorrect = checkAnswer(userResponse)
-    if(isCorrect) {
+    if (isCorrect) {
       confetti()
+      setError(null)
       updateQuestion()
     } else {
-      alert('equivocado')
+      pushNotify()
+      setError(true)
     }
-
   }
 
   const { alternativas } = question
@@ -58,7 +85,7 @@ function ListOfOptions({ question, checkAnswer, updateQuestion }) {
             userResponse={userResponse}
           />
         ))}
-        <button>Siguiente</button>
+        <button className="h-12 mt-4">Siguiente</button>
       </form>
     </>
   )
@@ -66,16 +93,22 @@ function ListOfOptions({ question, checkAnswer, updateQuestion }) {
 
 export default function App() {
   const [currentQuestion, setCurrentQuestion] = useState(null)
+  const [count, setCount] = useState(0)
+
+  const MAX_QUESTION = 10 
 
   const updateQuestion = () => {
     const questions = getQuestions()
     const randomIndex = Math.floor(Math.random() * 200)
 
     setCurrentQuestion(questions[randomIndex])
+    setCount(count + 1)
   }
 
   const checkAnswer = (userResponse) => {
-    return  userResponse === currentQuestion.alternativas[currentQuestion.respuesta]
+    return (
+      userResponse === currentQuestion.alternativas[currentQuestion.respuesta]
+    )
   }
 
   useEffect(() => {
@@ -84,10 +117,14 @@ export default function App() {
 
   if (!currentQuestion) return "Loading"
 
+  if(count > MAX_QUESTION) {
+    return "Se termino"
+  }
+
   return (
     <div className="max-w-xl mx-auto w-full ">
       <header>
-        <h3 className="text-xl font-bold">Neyda ❤️preguntas</h3>
+        <h3 className="text-xl font-bold">Pregunta {count + '/' + MAX_QUESTION}</h3>
       </header>
       <article className="flex flex-col justify-center">
         <header>
@@ -100,7 +137,11 @@ export default function App() {
             </div>
           )}
         </header>
-        <ListOfOptions question={currentQuestion} checkAnswer={checkAnswer} updateQuestion={updateQuestion} />
+        <ListOfOptions
+          question={currentQuestion}
+          checkAnswer={checkAnswer}
+          updateQuestion={updateQuestion}
+        />
       </article>
     </div>
   )
